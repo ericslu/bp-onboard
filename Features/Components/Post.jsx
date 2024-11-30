3f533288127487345c8cd0c4155e5dd283923c19
Eric Lu <84652493+ericslu@users.noreply.github.com>
2024-11-30 02:24:51 -0800

    add multiple component rendering, prop type validation, post

---
+++ Features/Components/Post.jsx
@@ -0,0 +1,25 @@
+// Features/Feed/Components/Post.jsx
+import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function Post({ username, body }) {
+  return (
+    <>
+      <Text>
+        Author:
+        {' '}
+        {username}
+      </Text>
+      <Text>
+        Body:
+        {' '}
+        {body}
+      </Text>
+    </>
+  );
+}
+
+Post.propTypes = {
+  username: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
